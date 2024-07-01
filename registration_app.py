f8459af3f4832bad5f7fed515d38ca63f42eca96
fokhruli <fokhrul.rmedu@gmail.com>
2024-07-01 15:35:06 +0600

    local and remote branches aligned

--- registration_app.py
+++ registration_app.py
@@ -50,4 +50,4 @@ async def checkout_report(input_date: DateModel, num_people: int = 5):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=6970)
