8a6967bb1f50e77632729e4260a47d790e486a5e
fokhruli <fokhrul.rmedu@gmail.com>
2024-07-16 12:32:50 +0600

    hitcount added

--- registration_app.py
+++ registration_app.py
@@ -16,6 +16,51 @@ class DateModel(BaseModel):
     date_field: date =  Field(..., description="Pick a date")
 
 
+from datetime import datetime
+import logging
+import uuid
+import time, os
+from fastapi import Request
+
+projcet_name = 'DUMMY_PROJECT_FACE_RECOG'
+
+if not os.path.exists('API_REQUEST_LOGS'):
+    os.mkdir('API_REQUEST_LOGS')
+    
+# Set up logging
+logging.basicConfig(
+    filename='API_REQUEST_LOGS/api_access.log',
+    level=logging.INFO,
+    format='%(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S'
+)
+
+def generate_unique_id():
+    # Combine current timestamp (in microseconds) with a UUID
+    timestamp = int(time.time() * 1000000)
+    random_string = str(uuid.uuid4()).replace('-', '')
+    return f"{timestamp}-{random_string}"
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    unique_id = generate_unique_id()
+    # Log the request
+    logging.info(
+        "ENDPOINT_REQUEST_FOUND: "
+        "{"
+        f"'Unique_ID': '{unique_id}', "
+        f"'Project_Name': '{projcet_name}', "
+        f"'Endpoint_Name': '{str(request.url.path)}', "
+        f"'Client_IP': '{request.client.host}', "
+        f"'Request_Time': '{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}'"
+        "}"
+    )
+    
+    # Call the next middleware or route handler
+    response = await call_next(request)
+    
+    return response
+
 
 @app.post("/registration/")
 
@@ -50,4 +95,4 @@ async def checkout_report(input_date: DateModel, num_people: int = 5):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=6970)
+    uvicorn.run(app, host="0.0.0.0", port=6979)
